890ac4bef2b3f383e13aaab03b0227bf5e805cfa
JOJOWWWW <jojo.bandayrel@lorma.edu>
2025-04-26 16:05:03 +0800

    from figma to flutter

--- fastapi_app/app/schemas.py
+++ fastapi_app/app/schemas.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 class FruitCreate(BaseModel): 
     name: str
     seedless: bool = True
+    image_path: Optional[str] = None
 
 class FruitOut(FruitCreate):
     id: int
